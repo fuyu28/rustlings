a34f81c4bcc9ac2f08eb3355140c69d1fef77ac6
fuyu <88916226+fuyu28@users.noreply.github.com>
2025-08-01 18:44:03 +0900

    01_variablesまで終わり

---
+++ exercises/01_variables/variables1.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // TODO: 抜けているキーワードを追加しましょう。
+    let x = 5;
+
+    println!("x has the value {x}");
+}
---
+++ exercises/01_variables/variables6.rs
@@ -0,0 +1,6 @@
+// TODO: 以下の行を変更し、コンパイルエラーを解消しましょう。
+const NUMBER: i32 = 3;
+
+fn main() {
+    println!("Number: {NUMBER}");
+}
